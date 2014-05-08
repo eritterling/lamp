bee8dbe20b51ce825705799e6461987d18d697a0
Zach Sullivan <zachsully@gmail.com>
2014-05-08 17:54:01 -0400

    updated physics; need to find a way to detect the floor

--- lamp.java
+++ lamp.java
@@ -23,14 +23,13 @@ class Lamp extends JFrame {
 // View holds the game
 class Game extends JComponent implements KeyListener,  MouseMotionListener, ActionListener {
     Player player;
-    Block level;
     Timer timer;
     
-    private final int FPS = 10;
+    private final int FPS = 45;
     private final int SPEED = 1000/FPS;
 
     Game() {
-	player = new Player(0,0);
+	player = new Player(0,0,0,0,0,2);
 	this.timer = new Timer(this.SPEED, this);
 	this.timer.start();
     }
@@ -41,13 +40,15 @@ class Game extends JComponent implements KeyListener,  MouseMotionListener, Acti
 
     // the action performed here is the the clock ticking
     public void actionPerformed(ActionEvent e) {
-	this.player.move(0.25);
+	this.player.move(.25);
 	System.out.println(this.player);
+	if (this.isValid(player)) {
+	} else this.timer.stop();
 	this.repaint();
     }
 
     public boolean isValid(Player x) {
-        if ( x.getX() < 400 && x.getX() >= 0 && x.getY() < 400 && x.getY() >= 0 ) {
+        if ( x.getX() < 380 && x.getX() >= 0 && x.getY() < 380 && x.getY() >= 0 ) {
 	    return true;
 	} else {
 	    return false;
@@ -56,12 +57,11 @@ class Game extends JComponent implements KeyListener,  MouseMotionListener, Acti
 
     //Controls below:
 
-
     public void keyPressed(KeyEvent e) {
-	int move = 5;
+	/*	int move = 5;
 	int key = e.getKeyCode();
 	if (key == 40) { // down
-	    this.player = new Player(this.player.getX(),this.player.getY() + move);
+	    this.player = new Player(this.player.getX(),this.player.getY() + move, this.player.getVX(), player.getVY(),);
 	    this.repaint();
 	} else if (key == 39) { // right
 	    this.player = new Player(this.player.getX() + move,this.player.getY());
@@ -72,7 +72,7 @@ class Game extends JComponent implements KeyListener,  MouseMotionListener, Acti
 	} else if (key == 37) { // left
 	    this.player = new Player(this.player.getX() - move , this.player.getY());
 	    this.repaint();
-	}
+	    }**/
     }
 
     public void keyReleased(KeyEvent e) {
@@ -93,49 +93,14 @@ class Game extends JComponent implements KeyListener,  MouseMotionListener, Acti
 }
 
 class Player {
-    // location with respect to two dimensions
-    private int x,y;
-    // velocity with respect to x and y
-    private double vx, vy;
-    // acceleration with respect to x and y
-    private double ax, ay;
-
-    // Constructors for Player
-
-    // the no args constructor for player puts a player at the origin and sets ay equal to gravity
-    Player() {
-	this.x = 0;
-	this.y = 0;
-	this.vx = 0;
-	this.vy = 0;
-	this.ax = 0;
-	this.ay = 9.81;
-    }
-
-    // if you create a player and only give it an x and y it will start with no velocity in any direction
-    // it will also start with ay = -9.81 as gravity, with no ax acceleration
-    Player(int x, int y) {
-	this.x  = x;
-	this.y  = y;
-	this.vx = 0;
-	this.vy = 0;
-	this.ax = 0;
-	this.ay = 9.81;
-    }
-
-    // this constructor sets the velocities and the location by what you give it
-    // acceleration in x is set to 0 and in y it is equal to gravity: -9.81
-    Player(int x, int y, double vx, double vy) {
-	this.x  = x;
-	this.y  = y;
-	this.vx = vx;
-	this.vy = vy;
-	this.ax = 0;
-	this.ay = 9.81;
-    }
+    private double x,y; // location
+    private double vx, vy; // velocity
+    private double ax, ay; // acceleration
+    private double fx, fy; // force
+    private double mass; // mass
 
     // this is a complet constructor for Player that takes an x,y,vx,vy,ax, and ay
-    Player(int x, int y, double vx, double vy, double ax, double ay) {
+    Player(double x, double y, double vx, double vy, double ax, double ay) {
 	this.x  = x;
 	this.y  = y;
 	this.vx = vx;
@@ -148,17 +113,48 @@ class Player {
     public String toString() {
 	String out = "";
 
-	out = out + "(" + this.x + "," + this.y + "," + this.vx + "," + this.vy + ")";
+	out = out + "[" + "loc: " + "(" + this.x + "," + this.y + ") ; "
+	    + "vel: " + "(" + this.vx + "," + this.vy + ") ; "
+	    + "acc: " + "(" + this.ax + "," + this.ay + ")]";
 
 	return out;
     }
 
-    public int getX() {
+    // Get variables
+    public double getX() {
 	return this.x;
     }
 
-    public int getY() {
+    public double getY() {
 	return this.y;
+    }
+
+    public double getVX() {
+	return this.vx;
+    }
+
+    public double getVY() {
+	return this.vy;
+    }
+
+    public double getAX() {
+	return this.ax;
+    }
+
+    public double getAY() {
+	return this.ay;
+    }
+
+    public double getFX() {
+	return this.fx;
+    }
+
+    public double getFY() {
+	return this.fy;
+    }
+
+    public double getMass() {
+	return this.mass;
     }
     
     public void move(double time) {
@@ -182,33 +178,6 @@ class Player {
 
     public void draw(Graphics g) {
 	// fillRect(x,y,w,h)
-	g.fillRect(this.x,this.y,20,20);
-    }
-}
-
-// a torch is the players weapon and sits in the Player's hand
-class Torch extends JComponent {
-    // pos relative to Player
-    private int x, y;
-    
-    // takes an init pos
-    Torch() {
-    }
-    
-    public void draw(Graphics g) {
-	g.fillOval(this.x,this.x+10,this.y,this.y+10);
-    }
-}
-
-// a block is a standard piece in the game
-class Block {
-    int x,y;
-    Color color;
-
-    Block() {
-
-    }
-
-    public void draw(Graphics g) {
+	g.fillRect((int) this.x, (int) this.y,20,20);
     }
 }
